2b71cfe8099237d165319692ab661c3071702888
Leslie Osorio <lesosorio17@gmail.com>
2018-12-02 05:03:02 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+
+  class Artist
+
+    attr_accessor :name, :songs
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @songs = []
+    end
+
+    def add_song(song)
+      @songs << song
+    end
+
+    def save
+      @@all << self
+    end
+
+    def self.find_or_create_by_name(name)
+      if self.find(name)
+        self.find(name)
+      else
+        self.create(name)
+      end
+    end 
+
+  end
